658373d47f2c88e942b4681eb9e60cd7d23a06a1
granddaifuku <>
2019-10-29 13:31:44 +0900

    First Commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="cft",
+    version="1.0.0",
+    description="codeforces command line tool",
+    author="granddaifuku",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "cft = cf_cli:main"
+        ]
+    },
+    install_requirements=[
+        "shutil", "requests"
+    ],
+    license='MIT'
+)
