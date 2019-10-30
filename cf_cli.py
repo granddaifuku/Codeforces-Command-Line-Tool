cd845f54e7dbab36df6886dd85529837d4af81fd
granddaifuku <>
2019-10-30 15:20:08 +0900

    Useable for every programming language

--- cf_cli.py
+++ cf_cli.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import glob
 import requests
 import shutil
 
@@ -12,7 +13,16 @@ class APIError(Exception):
     pass
 
 
-def getResultJson(url, params=None):
+def set_template():
+    global template
+    base_dir = os.path.dirname(__file__)
+    base_dir = base_dir + "/main*"
+    p = glob.glob(base_dir)
+    template_program = os.path.basename(os.path.normcase(p[0]))
+    template = template_program
+
+
+def get_result_json(url, params=None):
     req = requests.get(url + params)
     if req.status_code != 200:
         return APIError("HTTP {}".format(req.status_code))
@@ -20,8 +30,8 @@ def getResultJson(url, params=None):
         return req.json()
 
 
-def getContestName(id):
-    res = getResultJson(api_url, "contest.standings?contestId=" + str(id))
+def get_contest_name(contest_id):
+    res = get_result_json(api_url, "contest.standings?contestId=" + str(contest_id))
     if res["status"] != "OK":
         raise APIError("{}: {}".format(res["status"], res["comment"]))
     if not res["result"]:
@@ -36,8 +46,8 @@ def getContestName(id):
     return s
 
 
-def getProblemIndex(id):
-    res = getResultJson(api_url, "contest.standings?contestId=" + str(id))
+def get_problem_index(contest_id):
+    res = get_result_json(api_url, "contest.standings?contestId=" + str(contest_id))
     if res["status"] != "OK":
         raise APIError("{}: {}".format(res["status"], res["comment"]))
     if not res["result"]:
@@ -49,14 +59,14 @@ def getProblemIndex(id):
     return problem_index
 
 
-def createContestDir(contest_id):
+def create_contest_dir(contest_id):
     current_dir = os.getcwd()
     base_dir = os.path.dirname(__file__)
-    contest_name = getContestName(contest_id)
+    contest_name = get_contest_name(contest_id)
     new_dir_path = contest_name
     if not os.path.isdir(new_dir_path):
         os.makedirs(new_dir_path)
-    problem_index = getProblemIndex(contest_id)
+    problem_index = get_problem_index(contest_id)
     for index in problem_index:
         new_problem_dir_path = new_dir_path + "/" + index
         if os.path.exists(new_problem_dir_path):
@@ -65,7 +75,7 @@ def createContestDir(contest_id):
         os.makedirs(new_problem_dir_path)
         with open(os.path.join(new_problem_dir_path, "main.cpp"), 'w') as f:
             f.write(" ")
-        shutil.copyfile(base_dir + "/" + "main.cpp", new_problem_dir_path + "/main.cpp")
+        shutil.copyfile(base_dir + "/" + template, new_problem_dir_path + "/" + template)
         os.chdir(new_problem_dir_path)
         os.system('oj d ' + contest_url + str(contest_id) + "/problem/" + index)
         os.chdir(current_dir)
@@ -81,7 +91,7 @@ def submit(contest_id):
     os.system('oj s ' + contest_url + str(contest_id) + "/problem/" + index + " " + template)
 
 
-def showHelp():
+def show_help():
     print("HOW TO USE")
     print("Create contest directory : \"cft n (contest id)\"")
     print("Test your code           : \"cft t\"")
@@ -94,29 +104,31 @@ def main():
     args = sys.argv
     if len(args) == 1:
         print("Not Enough Args")
-        showHelp()
+        show_help()
         sys.exit()
     elif len(args) > 3:
         print("Too Much Args")
-        showHelp()
+        show_help()
         sys.exit()
     command = args[1]
     if command == 'n':
         contest_id = args[2]
         if len(args) != 3:
             print("Enter Correct Number of Args")
-            showHelp()
+            show_help()
             sys.exit()
-        createContestDir(contest_id)
+        create_contest_dir(contest_id)
     elif command == 't':
         test()
     elif command == 's':
         contest_id = args[2]
         if len(args) != 3:
             print("Enter Correct Number of Args")
-            showHelp()
+            show_help()
             sys.exit()
         submit(contest_id)
     else:
-        showHelp()
+        show_help()
         sys.exit()
+
+
